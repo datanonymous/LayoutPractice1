1221f6be384a71e70348695e208560631babda63
Alex Ko <ko.e.alexander@gmail.com>
2019-03-15 22:21:52 -0400

    trying to get tab layout to work with bottom navigation menu

--- app/src/main/java/ko/alex/layoutpractice1/MainActivity.java
+++ app/src/main/java/ko/alex/layoutpractice1/MainActivity.java
@@ -1,13 +1,21 @@
 package ko.alex.layoutpractice1;
 
+//import androidx.annotation.NonNull;
+
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mTextMessage = findViewById(R.id.message);
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -73,4 +87,61 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
+
+
+
+
+
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+//                    mTextMessage.setText("home");
+                    Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+//                    fragment = new OneFragment();
+//                    loadFragment(fragment);
+                    toolbar.setTitle("Frag1");
+                    return true;
+                case R.id.navigation_dashboard:
+//                    mTextMessage.setText("home");
+                    Toast.makeText(getApplicationContext(), "Dashboard", Toast.LENGTH_SHORT).show();
+//                    fragment = new TwoFragment();
+//                    loadFragment(fragment);
+                    toolbar.setTitle("Frag2");
+                    return true;
+                case R.id.navigation_notifications:
+//                    mTextMessage.setText("home");
+                    Toast.makeText(getApplicationContext(), "Notification", Toast.LENGTH_SHORT).show();
+//                    fragment = new ThreeFragment();
+//                    loadFragment(fragment);
+                    toolbar.setTitle("Frag3");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.viewpager, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+
+
+
+
+
+
 }
